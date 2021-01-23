648eb7ffa039139afb044afd587b17397b8459b5
Sarusman <60844500+daiynos@users.noreply.github.com>
2021-01-23 18:22:03 +0100

    Add files via upload

---
+++ fonction.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import sys
+
+print("Fonction avec variable 'x' : ")
+function=input()
+print("Intervalle fermé de 2 réel separé d'un espace : ")
+interval=input().split()
+interval=[int(i) for i in interval]
+
+
+
+def solve_f(function, intervalle):
+	x_range=[]
+	y_range=[]
+	for x in range(intervalle[0], intervalle[1]):
+		x_range.append(x)
+		y_range.append(eval(function))
+	return x_range, y_range
+
+
+def main(function, interval):
+	solved_x, solved_y=solve_f(function, interval)
+	plt.plot(solved_x, solved_y)
+	plt.show()
+
+try:
+	x=interval[0]
+	eval(function)
+	main(function, interval)
+except SyntaxError:
+	print("Forme de la fonction invalide")
+
+except NameError:
+	print("Une variable n'est pas defini : "+sys.exc_info()[1])
+
+except ZeroDivisionError:
+	print('Intervalle invalide : division par 0')
+
+except:
+	print("Erreur inconnu \n" +str(sys.exc_info()))
+
+
+
+
